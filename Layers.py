fa886648530fb00422dfe4e1c2c5bfb1c66b7925
coldtaco <36543008+coldtaco@users.noreply.github.com>
2022-11-24 16:04:11 +1100

    Add model

--- Layers.py
+++ Layers.py
@@ -111,3 +111,12 @@ class ReLU:
     def __call__(self, x):
         x[x<0] = 0
         return x
+
+class Model:
+    def __init__(self, layers):
+        self.layers = layers
+    
+    def __call__(self, x):
+        for l in self.layers:
+            x = l(x)
+        return x
