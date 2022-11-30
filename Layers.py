f3e85481088eb5fcd67c69c56006c2ae84b585c4
coldtaco <36543008+coldtaco@users.noreply.github.com>
2022-11-30 15:55:19 +1100

    added new convolution

--- Layers.py
+++ Layers.py
@@ -55,22 +55,28 @@ def pooling(mat,ksize,method='max',pad=False):
     return result
 
 class Conv2D:
-    def __init__(self, kernel, bias):
+    def __init__(self, kernel, bias, input_shape):
         self.kernel = kernel
         self.bias = bias
+        k1, k2, c_in, c_out = kernel.shape
+        n, h, w, c = input_shape
+
+        template = np.arange(h*w*c).reshape(1,h,w,c) # Template for 1D indexing of input image
+        snips = []
+        for y in range(h-(k1-1)):
+            for x in range(h-(k2-1)):
+                snip = template[0, y:y+k1, x:x+k2,:]
+                snips.append(snip)
+        self.img_inds = np.asarray(snips).reshape(-1, k1 * k2* c_in)
+        self.kernel = kernel.reshape(k1*k2*c_in, c_out)
+        self.output_shape = (-1, h-k1+1, w-k2+1, c_out)
 
     def __call__(self, img):
-        kernel = self.kernel
-        n, h, w, c = img.shape
-        k, k, c_i, c_o = kernel.shape
-        res = np.zeros((n, h-k+1, w-k+1, c_o))
-        for n_ in range(n):
-            for c_out in range(c_o):
-                for y in range(h-(k-1)):
-                    for x in range(h-(k-1)):
-                        snip = img[n_, y:y+k, x:x+k,:]
-                        res[n_, y,x,c_out] = np.sum(snip * kernel[:,:,:,c_out])
-        return res + self.bias
+        img = img.reshape(-1) # Flatten for indexing
+        img = img[self.img_inds]
+        img = img @ self.kernel
+        img = img.reshape(*self.output_shape)
+        return img + self.bias
 
 class MaxPool2D:
     def __init__(self, stride):
